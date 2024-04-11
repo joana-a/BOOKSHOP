c24193bd293c9815a8f4e984f3e270eef5e049d9
joana-a <[abamensah16@gmail.com]>
2024-04-11 16:27:30 +0000

    updated connection

--- settings/connection.php
+++ settings/connection.php
@@ -1,10 +1,14 @@
 <?php
-define("servername","localhost");
-define("username","root");
-define("password","");
-define("database", "joslibraryy");
+// define("servername","localhost");
+// define("username","root");
+// define("password","");
+// define("database", "joslibraryy");
 
-$mysqli= new mysqli(servername, username, password, database);
+$connectionstring= getenv("AZURE_MYSQL_CONNECT");
+
+// $connectionstring= env('AZURE_MYSQL_CONNECT');
+
+$mysqli= new mysqli(getenv("AZURE_MYSQL_HOST"), getenv("AZURE_MYSQL_USERNAME"), getenv("AZURE_MYSQL_PASSWORD"), getenv("AZURE_MYSQL_DBNAME"));
 
 if ($mysqli->connect_error) {
     echo("Connection failed: ". $mysqli->connect_error);
